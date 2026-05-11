9ae38e8d99d44c9e46afc99934a3ec3b816abe5d
gojeta786 <gokuvegetadbz111@gmail.com>
2026-05-11 20:45:36 +0530

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type CategoryInfo = any;
+export type Transaction = any;
+export type Budget = any;
+export type Insight = any;
+export type PeerComparison = any;
+export type DemographicInsights = any;
